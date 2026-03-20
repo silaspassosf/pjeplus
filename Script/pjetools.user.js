dfc2178aab0161287e386f614b6c1a8eaa79b913
silaspassosf <silas.passosf@gmail.com>
2026-03-20 01:38:13 -0300

    Bump pjetools.user.js to v2.1.4

--- Script/pjetools.user.js
+++ Script/pjetools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PJe Tools Pro
 // @namespace    http://tampermonkey.net/
-// @version      2.1.3
+// @version      2.1.4
 // @description  Suite de ferramentas para PJe
 // @author       Silas
 // ── PJe (cobre todas as rotas com um único match)
